79dd7c95ed4a62e111de62f64b1bbb714102877e
Duy Nguyễn <duykhanhchi1993@gmail.com>
2023-01-19 15:27:43 +0700

    add sidenav

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/resources/views/layout/*.{pug, html}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary-grey": "F7F6F9",
+        "dark-grey": "8A8B93",
+        "light-purple": "EFE9FF",
+        "strong-purple": "6324FE",
+      },
+    },
   },
   plugins: [],
 };
