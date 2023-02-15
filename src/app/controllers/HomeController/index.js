5897cbdfea808312c28b37781dbddca821469c15
Duy Nguyễn <duykhanhchi1993@gmail.com>
2023-02-16 02:58:44 +0700

    :sparkles: add login, signup page

--- src/app/controllers/HomeController/index.js
+++ src/app/controllers/HomeController/index.js
@@ -2,16 +2,25 @@ const connection = require("../../../config/db");
 
 class HomeController {
   index(req, res) {
-    const totalQuery =
-      "select COUNT(OrderID) orderNum from Orders; select COUNT(CustomerID) customerNum from Customer; select COUNT(ShipperID) shipperNum from Shipper";
-    connection.query(totalQuery, function (err, results) {
-      if (err) return err;
-      res.render("home", {
-        totalOrders: results[0][0].orderNum,
-        totalCustomers: results[1][0].customerNum,
-        totalShippers: results[2][0].shipperNum,
-      });
-    });
+    // const totalQuery =
+    //   "select COUNT(OrderID) orderNum from Orders; select COUNT(CustomerID) customerNum from Customer; select COUNT(ShipperID) shipperNum from Shipper";
+    // connection.query(totalQuery, function (err, results) {
+    //   if (err) return err;
+    //   res.render("home", {
+    //     totalOrders: results[0][0].orderNum,
+    //     totalCustomers: results[1][0].customerNum,
+    //     totalShippers: results[2][0].shipperNum,
+    //   });
+    // });
+    res.render("index");
+  }
+
+  login(req, res) {
+    res.render("auth/login");
+  }
+
+  register(req, res) {
+    res.render("auth/register");
   }
 }
 
--- src/routes/Home/index.js
+++ src/routes/Home/index.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const HomeController = require("../../app/controllers/HomeController");
 
 router.get("/", HomeController.index);
+router.get("/login", HomeController.login);
+router.get("/register", HomeController.register);
 
 module.exports = router;
